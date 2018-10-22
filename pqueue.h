dfb5e2400134d0a98ce1cbafc7ce5a7965213537
Karuna Grewal <karunagrewal98@gmail.com>
2018-10-22 18:09:51 +0530

    Add priority queue structures

---
+++ pqueue.h
@@ -0,0 +1,31 @@
+#ifndef _PQUEUE_H_
+#define _PQUEUE_H_
+
+#define MAX_PROCESSES 100
+typedef struct process process;
+struct process{
+    int pid;
+    int et;
+    int period;
+    int deadline;
+    int priority;
+};
+
+typedef struct pqueue pqueue;
+struct pqueue{
+    //note this
+    process **ready;
+    int pq_capacity;
+    int pq_size;
+};
+
+//insert
+void insert_process(pqueue *rdqueue, process *p);
+//delete
+void extract_process(pqueue *rdqueue, process *p);
+//decrease priority
+void dec_priority(pqueue *rdqueue, process *p);
+//getmax
+process *get_max(pqueue *rdqueue);
+
+#endif
