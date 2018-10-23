4111f979edbca219132d859f82b5f092c11f3819
Karuna Grewal <karunagrewal98@gmail.com>
2018-10-23 13:13:04 +0530

    Add hyperperiod calculation

--- EDF.c
+++ EDF.c
@@ -5,6 +5,26 @@
 #define MAXPROCESS 100
 process **global_processes;
 
+int
+get_gcd(int num1, int num2)
+{
+    if(num2 == 0)
+        return num1;
+    return get_gcd(num2, num1%num2);
+}
+
+int get_lcm(int nproc)
+{
+    int hyper_period = 1;
+    for(int i = 0; i < nproc; i++)
+    {
+        process *p2 = global_processes[i];
+        //printf("%d, ", p2->period);
+        hyper_period = (hyper_period * p2->period)/get_gcd(hyper_period, p2->period);
+    }
+    return hyper_period;
+}
+
 
 int main()
 {
@@ -23,5 +43,6 @@ int main()
         i++;
     }
     pqueue_display_process(ready_queue);
+    printf("%d", get_lcm(process_count));
     return 0;
 }
