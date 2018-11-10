68b9137fae5873ae1792df0dc33746bf16455f83
Karuna Grewal <karunagrewal98@gmail.com>
2018-11-11 01:58:58 +0530

    Modify EDF policy according to periodic task model

--- EDF.c
+++ EDF.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "pqueue.h"
+#include "pqueuev2.h"
 
 #define MAXPROCESS 100
-process **global_processes;
+task **global_tasks;
+static int pid_count = 0;
+static int task_count = 0;
 
 int
 get_gcd(int num1, int num2)
@@ -13,12 +15,12 @@ get_gcd(int num1, int num2)
     return get_gcd(num2, num1%num2);
 }
 
-int get_lcm(int nproc)
+int get_lcm()
 {
     int hyper_period = 1;
-    for(int i = 0; i < nproc; i++)
+    for(int i = 0; i < task_count; i++)
     {
-        process *p2 = global_processes[i];
+        task *p2 = global_tasks[i];
         //printf("%d, ", p2->period);
         hyper_period = (hyper_period * p2->period)/get_gcd(hyper_period, p2->period);
     }
@@ -30,12 +32,11 @@ check_arrivals(pqueue *rdqueue, int cur_time, int nproc)
 {
     for(int i = 0; i < nproc; i++)
     {
-        if(!global_processes[i]->ret && global_processes[i]->deadline/*period*/ <= cur_time)
+        if(global_tasks[i]->job_list == NULL && global_tasks[i]->deadline/*period*/ <= cur_time)
         {
-            global_processes[i]->ret = global_processes[i]->et;
-            global_processes[i]->deadline += global_processes[i]->deadline;
-            process *p = process_init(i+1, global_processes[i]->et, global_processes[i]->period, 
-                                global_processes[i]->deadline, global_processes[i]->period);
+            global_tasks[i]->deadline += global_tasks[i]->deadline;
+            process *p = process_init(pid_count++, global_tasks[i]->wcet, global_tasks[i]->period, 
+                                global_tasks[i]->task_id, global_tasks[i]);
             //FIXME :recreate the process this is reinsertion.
             pqueue_insert_process(rdqueue, p);
         }
@@ -48,7 +49,6 @@ schedule_edf(pqueue *rdqueue, int nproc, int hyperperiod)
 {
     //execute until 1 hyperperiod
     int cur_time = 0;
-    int prev_pid = -1;
     while(cur_time <= hyperperiod)
    {
         check_arrivals(rdqueue, cur_time, nproc);
@@ -60,8 +60,12 @@ schedule_edf(pqueue *rdqueue, int nproc, int hyperperiod)
             cur_proc->ret--;
             if(cur_proc->ret == 0)
             {
-                global_processes[cur_proc->pid - 1]->ret = 0;
+                FILE *log_file = fopen("sched-op-edf.txt", "a+");
+                fprintf(log_file, "task: %d pid:%d aet: %d\n", cur_proc->task_id, cur_proc->pid, cur_proc->aet);
+                fclose(log_file);
                 pqueue_extract_process(rdqueue, cur_proc);
+                //unlink from job lists
+                remove_job(cur_proc->task_ref, cur_proc);
             }
                 
         }
@@ -71,24 +75,34 @@ schedule_edf(pqueue *rdqueue, int nproc, int hyperperiod)
    }
 }
 
+pqueue *
+submit_processes()
+{
+    FILE *task_file = fopen("tasks-edf", "r");
+    int wcet, period, deadline;
+    int task_no = 0;
+    int process_count;
+    fscanf(task_file,"%d", &process_count);
+    pqueue *ready_queue = pqueue_init(process_count, MAXPROCESS);
+    global_tasks = (task**)malloc(process_count * sizeof(task*));
+    //wcet, period, deadline
+    while(fscanf(task_file, "%d, %d, %d", &wcet, &period, &deadline) == 3)
+    {
+        task *t = task_init(task_count++, wcet, period, deadline);
+        process *p = process_init(pid_count++, wcet, period, task_count, t);
+        task_submit_job(t, p);
+        global_tasks[task_no++] = t;
+        pqueue_insert_process(ready_queue, p);
+    }
+    fclose(task_file);
+    return ready_queue;
+}
+
 int main()
 {
-    int process_count;
-    int i = 0;
-    scanf("%d",&process_count);
-    pqueue *ready_queue = pqueue_init(process_count, MAXPROCESS);
-    global_processes = (process**)malloc(process_count * sizeof(process*));
-    while(i < process_count)
-    {
-        int pid, et, period, deadline;
-        scanf("%d %d %d", &et, &period, &deadline);
-        process *p = process_init(i+1, et, period, deadline, deadline);
-        global_processes[i] = p;
-        pqueue_insert_process(ready_queue, p);
-        i++;
-    }
+    pqueue *ready_queue = submit_processes();
     pqueue_display_process(ready_queue);
-    printf("%d", get_lcm(process_count));
-    schedule_edf(ready_queue, process_count, get_lcm(process_count));
+    printf("%d", get_lcm());
+    schedule_edf(ready_queue, task_count, get_lcm());
     return 0;
 }
